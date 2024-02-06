4e698b3cfe059580da47942ddb8a39991581d8da
adamnear <nearing14@hotmail.com>
2024-02-06 13:51:58 -0400

    push to github to get domain

---
+++ pages/allProgWork.js
@@ -0,0 +1,11 @@
+/*I want this to show all my work as ideally short, 
+looping videos of a run through of the page
+
+Work will include:
+    Relaxation space internship project (Javascript/React)
+    Three in a row game (Javascript)
+    Cupcake store/Record collection CRUD app (C#)
+    Weather app (Kotlin)
+
+
+*/
---
+++ pages/allVideoWork.js
@@ -0,0 +1,7 @@
+/*Show my video work in very short looping clips
+Work to include:
+    sevenbays vid x2
+    relaxation space x2
+    surface tension
+    yoga studio NZ
+*/
